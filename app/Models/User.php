4cb2645f654f8d21c548d2ec1d92736dc2bf99a9
Tun Tun <21522825@gm.uit.edu.vn>
2023-12-28 16:34:52 +0700

    up

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Traits\HandleImageTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use HandleImageTrait, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +25,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'address',
+        'gender',
     ];
 
     /**
@@ -42,4 +49,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+
+    // public function getImagePathAttribute(){
+    //     return asset($this->images->count() > 0 ? 'upload/' . $this->images->first()->url : 'upload/defaultuser.png');
+    // }
 }
