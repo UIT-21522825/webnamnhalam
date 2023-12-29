63948f6d8c43f084db6ca82ef3612750aa784e82
Tun Tun <21522825@gm.uit.edu.vn>
2023-12-29 17:13:46 +0700

    up

--- resources/views/client/layouts/navbar.blade.php
+++ resources/views/client/layouts/navbar.blade.php
@@ -53,7 +53,7 @@
                         @if (auth()->check())
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                                {{ __('Đăng xuất') }}
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST"
